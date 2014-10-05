524a23a6a52945040be74fd4db39daa0dae41f70
JJ <Admin@Admins-MBP-3.home>
2014-10-05 14:52:08 -0700

    Added in comments

--- ApplePayExample/ViewController.m
+++ ApplePayExample/ViewController.m
@@ -45,13 +45,20 @@
  */
 -(IBAction)handlesApplePay:(id)sender
 {
+    
     @try {
+        
         PKPaymentSummaryItem *paymentSummary = [PKPaymentSummaryItem summaryItemWithLabel:@"Credit Card" amount:[NSDecimalNumber decimalNumberWithString:@"20.00"]];
         
         NSArray *summaryItem = [[NSArray alloc] initWithObjects:paymentSummary, nil];
+        
+        //Creates the type of credit card it wil support
         NSArray *supportPaymentType = [[NSArray alloc] initWithObjects:PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa, nil];
         
         PKPaymentRequest *paymentRequest = [[PKPaymentRequest alloc]init];
+        
+        //This is the merchant id you create from the provisioing profile page
+        //developer.apple.com
         paymentRequest.merchantIdentifier = @"merchant.com.ApplePay";
         paymentRequest.paymentSummaryItems = summaryItem;
         paymentRequest.countryCode = @"EN";
