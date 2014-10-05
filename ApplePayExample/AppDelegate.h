44e19bab2961c162f9c486f0d94e890c39cb5b6c
JJ <Admin@Admins-MBP-3.home>
2014-10-05 14:27:52 -0700

    Initial Commit

---
+++ ApplePayExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayExample
+//
+//  Created by JJ on 10/5/14.
+//  Copyright (c) 2014 Jay Lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayExample
+//
+//  Created by JJ on 10/5/14.
+//  Copyright (c) 2014 Jay Lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
